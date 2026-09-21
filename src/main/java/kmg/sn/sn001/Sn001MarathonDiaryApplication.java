eb18558f58cf5ee5facb770e5658fda8d8878720
KenichiroArai <sammyleejunior@gmail.com>
2026-09-22 00:30:54 +0900

    #1 Spring Boot 4.1 / Java 25 の Web アプリ基盤を追加する
    Maven 座標 kmg.sn.sn001:sn001-marathon-diary:0.1.0 で最小構成を用意し、.gitignore に Maven/IDE 用エントリを追加する。
    
    (プロジェクトの準備 #1)

---
+++ src/main/java/kmg/sn/sn001/Sn001MarathonDiaryApplication.java
@@ -0,0 +1,12 @@
+package kmg.sn.sn001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sn001MarathonDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sn001MarathonDiaryApplication.class, args);
+    }
+}
---
+++ src/test/java/kmg/sn/sn001/Sn001MarathonDiaryApplicationTests.java
@@ -0,0 +1,12 @@
+package kmg.sn.sn001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sn001MarathonDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
